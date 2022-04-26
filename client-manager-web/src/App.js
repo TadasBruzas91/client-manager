a3a6aa3622a80a4124a815d14fa82fdd317b7862
Tadas Bružas <tadasbruzas91@gmail.com>
2022-04-26 09:30:00 +0000

    Create rounting in App component.

--- client-manager-web/src/App.js
+++ client-manager-web/src/App.js
@@ -1,5 +1,7 @@
 import NavBar from './navbar';
+import { Routes, Route } from 'react-router-dom';
 import { config } from './utils/configLoader';
+import Clients from './clients';
 import './App.css';
 
 function App() {
@@ -7,6 +9,11 @@ function App() {
     <>
       <NavBar links={config.navlinks} />
       <div className="App">
+        <Routes>
+          <Route path='/dashboard' element={null} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path='/' exact element={null} />
+        </Routes>
       </div>
     </>
   );
