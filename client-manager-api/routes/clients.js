c5903a3a8e51435e01164dc31f066800868b852f
Tadas Bružas <tadasbruzas91@gmail.com>
2022-04-27 20:45:27 +0300

    Create basic api functions.

--- client-manager-api/routes/clients.js
+++ client-manager-api/routes/clients.js
@@ -1,33 +1,42 @@
 const express = require("express")
 const router = express.Router()
+const Client = require("../schemas/clientSchema")
 
 // Get all clients
-router.get("/", (req, res) => {
-    res.json({ name: "Client" })
+router.get("/", async (req, res) => {
+    const { page, limit } = req.query
+    const clients = await Client.paginate({}, { page, limit })
+    res.json(clients)
 })
 
 // Get one client
-router.get("/:id", (req, res) => {
-    res.json({ id: req.params.id })
-})
-
-// Add new client
-router.post("/", (req, res) => {
-    const client = req.body
+router.get("/:id", async (req, res) => {
+    const { id } = req.params
+    const client = await Client.findById(id)
     res.json(client)
 })
 
+// Add new client
+router.post("/", async (req, res) => {
+    const client = new Client(req.body)
+    const result = await client.save()
+    res.json(result)
+})
+
 // Edit client
-router.post("/:id", (req, res) => {
+router.post("/:id", async (req, res) => {
     const { id } = req.params
-    const client = req.body
-    res.json({ ...client, id })
+    const client = await Client.findById(id)
+    client.set({ ...client, ...req.body })
+    const result = await client.save()
+    res.json(result)
 })
 
 // Delete client
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
     const { id } = req.params
-    res.send(id)
+    const result = await Client.findByIdAndDelete(id)
+    res.send(result)
 })
 
 module.exports = router
